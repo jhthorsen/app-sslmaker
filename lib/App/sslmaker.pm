c36a26cad51c65f8e12f7df2af6b9985ebb9e484
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-09-30 22:24:21 +0200

    Released version 0.06
           * Cannot build on Win32

--- lib/App/sslmaker.pm
+++ lib/App/sslmaker.pm
@@ -6,7 +6,7 @@ App::sslmaker - Be your own SSL certificate authority
 
 =head1 VERSION
 
-0.05
+0.06
 
 =head1 DESCRIPTION
 
@@ -102,7 +102,7 @@ use constant DEBUG => $ENV{SSLMAKER_DEBUG} ? 1 : 0;
 use constant DEFAULT_BITS => 4096;
 use constant DEFAULT_DAYS => 365;
 
-our $VERSION = '0.05';
+our $VERSION = '0.06';
 our $OPENSSL = $ENV{SSLMAKER_OPENSSL} || 'openssl';
 
 my @CONFIG_TEMPLATE_KEYS = qw( bits cert crl_days days home key );
