2da836e769fe3d01a870a7344233e830e62d1dec
Jan Henning Thorsen <jhthorsen@cpan.org>
2014-09-30 22:33:40 +0200

    Released version 0.07
             Status: Database error
             ======================
             module : App::sslmaker
             version: undef
             in file: App-sslmaker-0.06/lib/App/sslmaker.pm
             status : The PAUSE indexer could not store the indexing result in
                      the DB due the following error: C< Lock wait timeout
                      exceeded; try restarting transaction >. Please report the
                      case to the PAUSE admins at modules@perl.org.

--- lib/App/sslmaker.pm
+++ lib/App/sslmaker.pm
@@ -6,7 +6,7 @@ App::sslmaker - Be your own SSL certificate authority
 
 =head1 VERSION
 
-0.06
+0.07
 
 =head1 DESCRIPTION
 
@@ -102,7 +102,7 @@ use constant DEBUG => $ENV{SSLMAKER_DEBUG} ? 1 : 0;
 use constant DEFAULT_BITS => 4096;
 use constant DEFAULT_DAYS => 365;
 
-our $VERSION = '0.06';
+our $VERSION = '0.07';
 our $OPENSSL = $ENV{SSLMAKER_OPENSSL} || 'openssl';
 
 my @CONFIG_TEMPLATE_KEYS = qw( bits cert crl_days days home key );
