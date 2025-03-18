7e0942caaa1651ee7a80479c46796c0119cf3ea3
Jan Henning Thorsen <jan.henning@thorsen.pm>
2025-03-18 22:31:05 +0900

    Add support for uploading to CPAN

--- lib/App/sslmaker.pm
+++ lib/App/sslmaker.pm
@@ -11,7 +11,7 @@ use constant DEBUG        => $ENV{SSLMAKER_DEBUG} || 0;
 use constant DEFAULT_BITS => $ENV{SSLMAKER_BITS}  || 4096;
 use constant DEFAULT_DAYS => $ENV{SSLMAKER_DAYS}  || 365;
 
-our $VERSION = '0.20';
+our $VERSION = '0.01';
 our $OPENSSL = $ENV{SSLMAKER_OPENSSL} || 'openssl';
 
 my @CONFIG_TEMPLATE_KEYS = qw(bits cert crl_days days home key);
@@ -301,10 +301,6 @@ sub _render_template {
 =head1 NAME
 
 App::sslmaker - Be your own SSL certificate authority
-
-=head1 VERSION
-
-0.16
 
 =head1 DESCRIPTION
 
